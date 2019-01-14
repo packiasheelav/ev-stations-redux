0c725352b41f25e01d88f87426625e90336b56dc
Packia Sheela Varadhan <packiasheelav@gmail.com>
2019-01-14 16:24:38 +0200

    map loaded

--- src/container/EvStationsList/index.js
+++ src/container/EvStationsList/index.js
@@ -1,40 +1,81 @@
-import React, { Component } from "react";
-import { Container, ListGroup, ListGroupItem } from "reactstrap";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { connect } from "react-redux";
-import {
-  getStations,
-} from "../../actions/getStations";
-import PropTypes from "prop-types";
-import "./LoadStations.css";
+import React, { Component } from 'react';
+import LoadMap from '../LoadMap';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getStations } from '../../actions/getStations';
+import PropTypes from 'prop-types';
+import './LoadStations.css';
 
 class EvStationsList extends Component {
-  state = {
-    showDetails: false
-  };
-  componentDidMount() {
-    this.props.getStations();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			apiKey: 'AIzaSyCGfeG6ePnKR9c98Ubo-w3SNDTaGAiZbrM',
+			map: null,
+			mapConfig: {
+				center: { lat: 60.192059, lng: 24.945831 },
+				zoom: 13,
+				disableDefaultUI: true,
+				zoomControl: true
+			},
+			markers: [],
+			show: false,
+			dataStations: '',
+			dataStationsStatus: '',
+			stationDetailId: '',
+			statusDetail: '',
+			stationDetailInfo: '',
+			isLoading: true,
+			loading: false,
+			error: ''
+		};
+	}
 
-  render() {
-    const { stations } = this.props.stations;
-    console.log(this.props);
-    return (
-        <div>hi</div>
-    );
-  }
+	componentDidMount() {
+		this.props.getStations();
+		window.initMap = this.initMap;
+		this.addScriptTag(
+			'https://maps.googleapis.com/maps/api/js?key=' + this.state.apiKey + '&libraries=places&callback=initMap'
+		);
+	}
+	initMap = () => {
+		this.setState({
+			map: new window.google.maps.Map(this.refs.map, this.state.mapConfig)
+		});
+	};
+
+	addScriptTag(src) {
+		let ref = window.document.getElementsByTagName('script')[0];
+		let script = window.document.createElement('script');
+		script.src = src;
+		script.async = true;
+		ref.parentNode.insertBefore(script, ref);
+	}
+	// this.state.dataStations.map(dataStation =>
+	// 	this.createMarker(dataStation, this.GetStatusByStationId(dataStation.id))
+	// );
+
+	render() {
+		const { stations } = this.props.stations;
+		return (
+			<div>
+				<div ref="map" id="mapId" />
+			</div>
+		);
+	}
 }
 
 EvStationsList.propTypes = {
-  getStations: PropTypes.func.isRequired,
-//   student: PropTypes.object.isRequired
+	getStations: PropTypes.func.isRequired
+	//   student: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  stations: state.stations
+	stations: state.stations
 });
 
 export default connect(
-  mapStateToProps,
-  { getStations }
+	mapStateToProps,
+	{ getStations }
 )(EvStationsList);
---
+++ src/container/LoadMap/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const STATIONS_URL = 'https://api.virta.fi/v4/stations';
+const STATIONS_STATUS_URL = 'https://api.virta.fi/v4/stations/status';
+const STATION_URL = 'https://api.virta.fi/v4/stations/';
+
+class LoadMap extends Component {
+	render() {
+		return <div>hi</div>;
+	}
+}
+export default LoadMap;
